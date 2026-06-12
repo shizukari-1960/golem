15eb37c899e1a3c8eab9952c5d3b3d5244e807a3
shizukari-1960 <shizukari1960@gmail.com>
2026-06-13 01:24:29 +0800

    add upload prepare

--- sheet.py
+++ sheet.py
@@ -1,45 +1,37 @@
-import aiohttp
-import asyncio
-import csv
-import pandas as pd
-from pprint import pprint
+import json
+import os
 
-async def fetch_data(doc_code, gid):
-    url = f"https://docs.google.com/spreadsheets/d/{doc_code}/export"
-    params = {
-        "format": "csv",
-        "gid": gid
-    }
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, headers=headers) as response:
-            if response.status == 200:
-                csv_data = await response.text(encoding='utf-8')
-                return csv_data
-            else:
-                raise Exception(f"Failed to fetch data: {response.status}")
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-def csv_to_df(csv_data):
-    from io import StringIO
-    df = pd.read_csv(StringIO(csv_data))
-    return df
-
-def modify_df(df):
-    chr_dict = df.to_dict(orient='list')
-    not_list_cols = ['player', 'age', 'gender', 'adven_class', 'belief', 'chr_name', 'race', 'reputation_total','reputation_used','soulscar', 'total_abi','total_exp','used_exp']
-    list_cols = [col for col in chr_dict.keys() if col not in not_list_cols]
-    for col in not_list_cols:
-        if col in chr_dict:
-            chr_dict[col] = chr_dict[col][0]
-    for col in list_cols:
-        chr_dict[col] = [x for x in chr_dict[col] if pd.notna(x)]
-    return chr_dict
+class Character:
+    def __init__(self, file_path:str):
+        self.file_path = file_path
+        with open(self.file_path, 'r') as f:
+            data = json.load(f)
+            self.ability = data.get('ability', None)
+            self.adven_class = data.get('adven_class', None)
+            self.age = int(data.get('age', 0))
+            self.belief = data.get('belief', None)
+            self.chr_name = data.get('chr_name', None)
+            self.gender = data.get('gender', None)
+            self.growth = data.get('growth', None)
+            self.player = data.get('player', None)
+            self.race = data.get('race', None)
+            self.reputation_total = int(data.get('reputation_total', 0))
+            self.reputation_used = int(data.get('reputation_used', 0))
+            self.skills = data.get('skills', None)
+            self.soulscar = int(data.get('soulscar', 0))
+            self.total_abi = int(data.get('total_abi', 0))
+            self.total_exp = int(data.get('total_exp', 0))
+            self.used_exp = int(data.get('used_exp', 0))
+            self.level = max(self.skills.values()) if self.skills else 0
+    
+    def __str__(self):
+        return f"Lv.{self.level} {self.chr_name}\nPlayer: {self.player}"
+    
+    def to_csv_row(self):
+        #chr_name, player, race, belief, age, gender, adv_class, level, used_exp
+        return f"{self.chr_name},{self.player},{self.race},{self.belief},{self.age},{self.gender},{self.adven_class},{self.level},{self.used_exp}\n"
+        
     
     
-if __name__ == "__main__":
-    csv_data = asyncio.run(fetch_data("19Eeq9w9xvTFUFZNcwt6XA_ztwAwX7GDLUcbAYIt_hKw", "1236171943"))
-    data = csv_to_df(csv_data)
-    data = modify_df(data)
-    pprint(data)
